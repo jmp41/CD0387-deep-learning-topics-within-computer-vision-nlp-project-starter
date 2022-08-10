b639986a1673146a242c0c0672b9947fd51a4a7e
jmp41 <jianming_pan@berkeley.edu>
2022-08-10 13:49:05 +0000

    1.1
    Modified hpo.py

--- hpo.py
+++ hpo.py
@@ -7,16 +7,36 @@ import torch.optim as optim
 import torchvision
 import torchvision.models as models
 import torchvision.transforms as transforms
+import smdebug.pytorch as smd
 
 import argparse
 
-def test(model, test_loader):
+def test(model, test_loader, criterion):
     '''
     TODO: Complete this function that can take a model and a 
           testing data loader and will get the test accuray/loss of the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    model.eval()
+    hook.set_mode(smd.modes.EVAL)
+    running_loss=0
+    running_corrects=0
+    with torch.no_grad():
+        for data, labels in test_loader:
+            outputs = model(data)
+            loss=criterion(outputs, labels)
+            _, preds = torch.max(outputs, 1)
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data).item()
+
+    total_loss = running_loss / len(test_loader)
+    total_acc = running_corrects / len(test_loader)
+
+    print(
+        "\nTest set: Average Loss: {:.4f}, Total Accuracy: {}/{} ({:.0f}%)\n".format(
+            total_loss, running_corrects, len(test_loader.dataset), total_acc
+        )
+    )
 
 def train(model, train_loader, criterion, optimizer):
     '''
@@ -24,14 +44,61 @@ def train(model, train_loader, criterion, optimizer):
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    model.train()
+    hook.set_mode(smd.modes.TRAIN)
+    
+    
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    loss.item(),
+                )
+            )
+    
+ for e in range(epoch):
+     running_loss=0
+     correct=0
+     for data, target in train_loader:
+         data=data.to(device)
+         target=target.to(device)
+         optimizer.zero_grad()
+         pred = model(data)             #No need to reshape data since CNNs take image inputs
+         loss = cost(pred, target)
+         running_loss+=loss
+         loss.backward()
+         optimizer.step()
+         pred=pred.argmax(dim=1, keepdim=True)
+         correct += pred.eq(target.view_as(pred)).sum().item()
+     print(f"Epoch {e}: Loss {running_loss/len(train_loader.dataset)}, \
+         Accuracy {100*(correct/len(train_loader.dataset))}%")
+    
     
 def net():
     '''
     TODO: Complete this function that initializes your model
           Remember to use a pretrained model
     '''
-    pass
+    model = models.resnet18(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False   
+
+    num_features=model.fc.in_features
+    model.fc = nn.Sequential(
+                   nn.Linear(num_features, 133),
+                   nn.softmax()
+    )
+    return model
 
 def create_data_loaders(data, batch_size):
     '''
@@ -45,7 +112,10 @@ def main(args):
     TODO: Initialize a model by calling the net function
     '''
     model=net()
-    
+
+    hook = smd.Hook.create_from_json_file()
+    hook.register_hook(model)
+
     '''
     TODO: Create your loss and optimizer
     '''
